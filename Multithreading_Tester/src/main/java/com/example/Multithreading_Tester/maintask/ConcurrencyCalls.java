7fb4cba6a974b283eb987e6766b3ee0025fe8a30
audd-ho ext_laptop_temp <audr0021@e.ntu.edu.sg>
2025-07-17 00:58:43 +0800

    ConcurrencyCalls split into general test case/call functions and actual calling of functions, frontend server calling test functions all set up and ready to run to get results

---
+++ Multithreading_Tester/src/main/java/com/example/Multithreading_Tester/maintask/ConcurrencyCalls.java
@@ -0,0 +1,111 @@
+package com.example.Multithreading_Tester.maintask;
+
+import com.example.Multithreading_Tester.enums.Nations;
+import com.example.Multithreading_Tester.service.dto.request.PersonRequestDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Function;
+
+@Component
+public class ConcurrencyCalls {
+
+    private final ExecutorService executorService;
+    public ConcurrencyCalls() {
+        this.executorService = Executors.newFixedThreadPool(20);
+    }
+
+    public <NationalityResponse,PersonResponse,PeopleemailResponse> void GeneralTesting(Function<Nations,NationalityResponse> nationality, Function<PersonRequestDTO,PersonResponse> person, Function<Nations,PeopleemailResponse> peopleemail) {
+        System.out.println("Running GeneralTesting Concurrently");
+
+        List<CompletableFuture<NationalityResponse>> nationalityResponses = new ArrayList<>();
+        List<CompletableFuture<PersonResponse>> personResponses = new ArrayList<>();
+        List<CompletableFuture<PeopleemailResponse>> peopleemailResponses = new ArrayList<>();
+
+        long time0 = System.currentTimeMillis();
+
+        nationalityResponses.add(CompletableFuture.supplyAsync(() -> nationality.apply(Nations.SG), this.executorService));
+        personResponses.add(CompletableFuture.supplyAsync(() -> person.apply(new PersonRequestDTO("987-56-3201", "509823641", Nations.US)), this.executorService));
+        peopleemailResponses.add(CompletableFuture.supplyAsync(() -> peopleemail.apply(Nations.SG), this.executorService));
+        for (Nations nat : Nations.values()) {
+            nationalityResponses.add(CompletableFuture.supplyAsync(() -> nationality.apply(nat), this.executorService));
+        }
+        for (Nations nat : Nations.values()) {
+            peopleemailResponses.add(CompletableFuture.supplyAsync(() -> peopleemail.apply(nat), this.executorService));
+        }
+        personResponses.add(CompletableFuture.supplyAsync(() -> person.apply(new PersonRequestDTO("987-56-3201", "509823641", Nations.US)), this.executorService));
+
+        // type inference using var, still statically typed but inferred during compile time here using "var"
+        var nationalityDone = CompletableFuture.allOf(nationalityResponses.toArray(new CompletableFuture[0]));
+        var personDone = CompletableFuture.allOf(personResponses.toArray(new CompletableFuture[0]));
+        var peopleemailDone = CompletableFuture.allOf(peopleemailResponses.toArray(new CompletableFuture[0]));
+
+        try {
+            CompletableFuture.allOf(nationalityDone, personDone, peopleemailDone).join();
+        } catch (CompletionException e) {
+            Throwable cause = e.getCause();
+            System.out.println(cause.getMessage());
+            System.exit(1);
+        }
+
+        long time1 = System.currentTimeMillis();
+
+        long time_taken = time1-time0;
+        System.out.println("GeneralTesting Concurrently: " + time_taken);
+    }
+
+    public <T> T Println_SideEffect(T result) {
+        System.out.println(result);
+        return result;
+    }
+
+    public <NationalityResponse,PersonResponse,PeopleemailResponse> void Println_GeneralTesting(Function<Nations,NationalityResponse> nationality, Function<PersonRequestDTO,PersonResponse> person, Function<Nations,PeopleemailResponse> peopleemail) {
+        System.out.println("Running Println_GeneralTesting Concurrently");
+
+        List<CompletableFuture<NationalityResponse>> nationalityResponses = new ArrayList<>();
+        List<CompletableFuture<PersonResponse>> personResponses = new ArrayList<>();
+        List<CompletableFuture<PeopleemailResponse>> peopleemailResponses = new ArrayList<>();
+
+        long time0 = System.currentTimeMillis();
+
+        nationalityResponses.add(CompletableFuture.supplyAsync(() -> nationality.apply(Nations.SG), this.executorService)
+                .thenApply(this::Println_SideEffect));
+        personResponses.add(CompletableFuture.supplyAsync(() -> person.apply(new PersonRequestDTO("987-56-3201", "509823641", Nations.US)), this.executorService)
+                .thenApply(this::Println_SideEffect));
+        peopleemailResponses.add(CompletableFuture.supplyAsync(() -> peopleemail.apply(Nations.SG), this.executorService)
+                .thenApply(this::Println_SideEffect));
+        for (Nations nat : Nations.values()) {
+            nationalityResponses.add(CompletableFuture.supplyAsync(() -> nationality.apply(nat), this.executorService)
+                    .thenApply(this::Println_SideEffect));
+        }
+        for (Nations nat : Nations.values()) {
+            peopleemailResponses.add(CompletableFuture.supplyAsync(() -> peopleemail.apply(nat), this.executorService)
+                    .thenApply(this::Println_SideEffect));
+        }
+        personResponses.add(CompletableFuture.supplyAsync(() -> person.apply(new PersonRequestDTO("987-56-3201", "509823641", Nations.US)), this.executorService)
+                .thenApply(this::Println_SideEffect));
+
+        CompletableFuture<Void> nationalityDone = CompletableFuture.allOf(nationalityResponses.toArray(new CompletableFuture[0]));
+        CompletableFuture<Void> personDone = CompletableFuture.allOf(personResponses.toArray(new CompletableFuture[0]));
+        CompletableFuture<Void> peopleemailDone = CompletableFuture.allOf(peopleemailResponses.toArray(new CompletableFuture[0]));
+
+        try {
+            CompletableFuture.allOf(nationalityDone, personDone, peopleemailDone).join();
+        } catch (CompletionException e) {
+            Throwable cause = e.getCause();
+            System.out.println(cause.getMessage());
+            System.exit(1);
+        }
+
+        long time1 = System.currentTimeMillis();
+
+        long time_taken = time1-time0;
+        System.out.println("Println_GeneralTesting Concurrently: " + time_taken);
+    }
+
+}
--- Multithreading_Tester/src/main/java/com/example/Multithreading_Tester/maintask/ConcurrencyCallsTest.java
+++ Multithreading_Tester/src/main/java/com/example/Multithreading_Tester/maintask/ConcurrencyCallsTest.java
@@ -1,109 +1,152 @@
 package com.example.Multithreading_Tester.maintask;
 
-import com.example.Multithreading_Tester.enums.Nations;
-import com.example.Multithreading_Tester.service.dto.request.PersonRequestDTO;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.*;
-import java.util.function.Function;
+import com.example.Multithreading_Tester.service.gRPC_related.*;
+import com.example.Multithreading_Tester.service.REST_related.protobuf_related.*;
+import com.example.Multithreading_Tester.service.REST_related.non_protobuf_related.*;
 
 @Component
-public class ConcurrencyCallsTest {
+public class ConcurrencyCallsTest implements CommandLineRunner {
 
-    private final ExecutorService executorService;
-    public ConcurrencyCallsTest() {
-        this.executorService = Executors.newFixedThreadPool(20);
+    private final ConcurrencyCalls concurrencyCalls;
+
+    private final Stub_gRPC_people_PeopleInfo_ServiceStub stubGRPCPeoplePeopleInfoServiceStub;
+    private final Stub_gRPC_peopleimage_PeopleInfo_ServiceStub stubGRPCPeopleimagePeopleInfoServiceStub;
+    private final Stub_gRPC_peopleimage_PeopleInfoB_ServiceStub stubGRPCPeopleimagePeopleInfoBServiceStub;
+
+    private final PeopleProtoService peopleProtoService;
+    private final PeopleImageStringProtoService peopleImageStringProtoService;
+    private final PeopleImageBytesProtoService peopleImageBytesProtoService;
+
+    private final PeopleService peopleService;
+    private final PeopleImageService peopleImageService;
+
+    public ConcurrencyCallsTest(
+            ConcurrencyCalls concurrencyCalls,
+
+            Stub_gRPC_people_PeopleInfo_ServiceStub stubGRPCPeoplePeopleInfoServiceStub,
+            Stub_gRPC_peopleimage_PeopleInfo_ServiceStub stubGRPCPeopleimagePeopleInfoServiceStub,
+            Stub_gRPC_peopleimage_PeopleInfoB_ServiceStub stubGRPCPeopleimagePeopleInfoBServiceStub,
+
+            PeopleProtoService peopleProtoService,
+            PeopleImageStringProtoService peopleImageStringProtoService,
+            PeopleImageBytesProtoService peopleImageBytesProtoService,
+
+            PeopleService peopleService,
+            PeopleImageService peopleImageService
+    ) {
+        this.concurrencyCalls = concurrencyCalls;
+
+        this.stubGRPCPeoplePeopleInfoServiceStub = stubGRPCPeoplePeopleInfoServiceStub;
+        this.stubGRPCPeopleimagePeopleInfoServiceStub = stubGRPCPeopleimagePeopleInfoServiceStub;
+        this.stubGRPCPeopleimagePeopleInfoBServiceStub = stubGRPCPeopleimagePeopleInfoBServiceStub;
+
+        this.peopleProtoService = peopleProtoService;
+        this.peopleImageStringProtoService = peopleImageStringProtoService;
+        this.peopleImageBytesProtoService = peopleImageBytesProtoService;
+
+        this.peopleService = peopleService;
+        this.peopleImageService = peopleImageService;
     }
 
-    public <NationalityResponse,PersonResponse,PeopleemailResponse> void GeneralTesting(Function<Nations,NationalityResponse> nationality, Function<PersonRequestDTO,PersonResponse> person, Function<Nations,PeopleemailResponse> peopleemail) {
-        System.out.println("Running GeneralTesting Concurrently");
+    @Override
+    public void run(String... args) throws Exception {
 
-        List<CompletableFuture<NationalityResponse>> nationalityResponses = new ArrayList<>();
-        List<CompletableFuture<PersonResponse>> personResponses = new ArrayList<>();
-        List<CompletableFuture<PeopleemailResponse>> peopleemailResponses = new ArrayList<>();
+        System.out.println("Pre-actual, Caching - START");
+        
+        concurrencyCalls.GeneralTesting(stubGRPCPeoplePeopleInfoServiceStub::nationality, stubGRPCPeoplePeopleInfoServiceStub::person, stubGRPCPeoplePeopleInfoServiceStub::peopleEmail);
+        concurrencyCalls.GeneralTesting(stubGRPCPeopleimagePeopleInfoServiceStub::nationalityImage, stubGRPCPeopleimagePeopleInfoServiceStub::personImage, stubGRPCPeopleimagePeopleInfoServiceStub::peopleEmailImage);
+        concurrencyCalls.GeneralTesting(stubGRPCPeopleimagePeopleInfoServiceStub::nationalityImageBytes, stubGRPCPeopleimagePeopleInfoServiceStub::personImageBytes, stubGRPCPeopleimagePeopleInfoServiceStub::peopleEmailImageBytes);
+        concurrencyCalls.GeneralTesting(stubGRPCPeopleimagePeopleInfoBServiceStub::nationalityImageBytes, stubGRPCPeopleimagePeopleInfoBServiceStub::personImageBytes, stubGRPCPeopleimagePeopleInfoBServiceStub::peopleEmailImageBytes);
 
-        long time0 = System.currentTimeMillis();
+        concurrencyCalls.GeneralTesting(peopleProtoService::nationalityJSON, peopleProtoService::personJSON, peopleProtoService::peopleEmailJSON);
+        concurrencyCalls.GeneralTesting(peopleProtoService::protoNationality, peopleProtoService::protoPerson, peopleProtoService::protoPeopleEmail);
+        concurrencyCalls.GeneralTesting(peopleImageStringProtoService::nationalityImageJSON, peopleImageStringProtoService::personImageJSON, peopleImageStringProtoService::peopleEmailImageJSON);
+        concurrencyCalls.GeneralTesting(peopleImageStringProtoService::protoNationalityImage, peopleImageStringProtoService::protoPersonImage, peopleImageStringProtoService::protoPeopleEmailImage);
+        concurrencyCalls.GeneralTesting(peopleImageBytesProtoService::nationalityImageJSON, peopleImageBytesProtoService::personImageJSON, peopleImageBytesProtoService::peopleEmailImageJSON);
+        concurrencyCalls.GeneralTesting(peopleImageBytesProtoService::protoNationalityImage, peopleImageBytesProtoService::protoPersonImage, peopleImageBytesProtoService::protoPeopleEmailImage);
+        concurrencyCalls.GeneralTesting(peopleImageBytesProtoService::nationalityImageJSON_PIR, peopleImageBytesProtoService::personImageJSON_PIR, peopleImageBytesProtoService::peopleEmailImageJSON_PIR);
+        concurrencyCalls.GeneralTesting(peopleImageBytesProtoService::protoNationalityImage_PIR, peopleImageBytesProtoService::protoPersonImage_PIR, peopleImageBytesProtoService::protoPeopleEmailImage_PIR);
 
-        nationalityResponses.add(CompletableFuture.supplyAsync(() -> nationality.apply(Nations.SG), this.executorService));
-        personResponses.add(CompletableFuture.supplyAsync(() -> person.apply(new PersonRequestDTO("987-56-3201", "509823641", Nations.US)), this.executorService));
-        peopleemailResponses.add(CompletableFuture.supplyAsync(() -> peopleemail.apply(Nations.SG), this.executorService));
-        for (Nations nat : Nations.values()) {
-            nationalityResponses.add(CompletableFuture.supplyAsync(() -> nationality.apply(nat), this.executorService));
+        concurrencyCalls.GeneralTesting(peopleService::nationality, peopleService::person, peopleService::peopleEmail);
+        concurrencyCalls.GeneralTesting(peopleService::nationalityJSON, peopleService::person, peopleService::peopleEmailJSON);
+        concurrencyCalls.GeneralTesting(peopleImageService::nationalityImageJSON, peopleImageService::personImage, peopleImageService::peopleEmailImageJSON);
+        
+        System.out.println("Pre-actual, Caching - END");
+        
+        
+        /**/
+
+
+        for (int i = 0; i < 50; i++) {
+            System.out.println("stubGRPCPeoplePeopleInfoServiceStub P-R");
+            concurrencyCalls.GeneralTesting(stubGRPCPeoplePeopleInfoServiceStub::nationality, stubGRPCPeoplePeopleInfoServiceStub::person, stubGRPCPeoplePeopleInfoServiceStub::peopleEmail);
+            System.out.println("END");
+
+            System.out.println("stubGRPCPeopleimagePeopleInfoServiceStub PI-R protobuf-String");
+            concurrencyCalls.GeneralTesting(stubGRPCPeopleimagePeopleInfoServiceStub::nationalityImage, stubGRPCPeopleimagePeopleInfoServiceStub::personImage, stubGRPCPeopleimagePeopleInfoServiceStub::peopleEmailImage);
+            System.out.println("END");
+
+            System.out.println("stubGRPCPeopleimagePeopleInfoServiceStub PI-R protobuf-Bytes");
+            concurrencyCalls.GeneralTesting(stubGRPCPeopleimagePeopleInfoServiceStub::nationalityImageBytes, stubGRPCPeopleimagePeopleInfoServiceStub::personImageBytes, stubGRPCPeopleimagePeopleInfoServiceStub::peopleEmailImageBytes);
+            System.out.println("END");
+
+            System.out.println("stubGRPCPeopleimagePeopleInfoBServiceStub PIB-R protobuf-Bytes");
+            concurrencyCalls.GeneralTesting(stubGRPCPeopleimagePeopleInfoBServiceStub::nationalityImageBytes, stubGRPCPeopleimagePeopleInfoBServiceStub::personImageBytes, stubGRPCPeopleimagePeopleInfoBServiceStub::peopleEmailImageBytes);
+            System.out.println("END");
+            
+            //
+
+            System.out.println("peopleProtoService (JSON send)");
+            concurrencyCalls.GeneralTesting(peopleProtoService::nationalityJSON, peopleProtoService::personJSON, peopleProtoService::peopleEmailJSON);
+            System.out.println("END");
+
+            System.out.println("peopleProtoService (Full protobuf)");
+            concurrencyCalls.GeneralTesting(peopleProtoService::protoNationality, peopleProtoService::protoPerson, peopleProtoService::protoPeopleEmail);
+            System.out.println("END");
+
+            System.out.println("peopleImageStringProtoService (JSON send)");
+            concurrencyCalls.GeneralTesting(peopleImageStringProtoService::nationalityImageJSON, peopleImageStringProtoService::personImageJSON, peopleImageStringProtoService::peopleEmailImageJSON);
+            System.out.println("END");
+
+            System.out.println("peopleImageStringProtoService (Full protobuf)");
+            concurrencyCalls.GeneralTesting(peopleImageStringProtoService::protoNationalityImage, peopleImageStringProtoService::protoPersonImage, peopleImageStringProtoService::protoPeopleEmailImage);
+            System.out.println("END");
+
+            System.out.println("peopleImageBytesProtoService (JSON send)");
+            concurrencyCalls.GeneralTesting(peopleImageBytesProtoService::nationalityImageJSON, peopleImageBytesProtoService::personImageJSON, peopleImageBytesProtoService::peopleEmailImageJSON);
+            System.out.println("END");
+
+            System.out.println("peopleImageBytesProtoService (Full protobuf)");
+            concurrencyCalls.GeneralTesting(peopleImageBytesProtoService::protoNationalityImage, peopleImageBytesProtoService::protoPersonImage, peopleImageBytesProtoService::protoPeopleEmailImage);
+            System.out.println("END");
+
+            System.out.println("peopleImageBytesProtoService, PIR (JSON send)");
+            concurrencyCalls.GeneralTesting(peopleImageBytesProtoService::nationalityImageJSON_PIR, peopleImageBytesProtoService::personImageJSON_PIR, peopleImageBytesProtoService::peopleEmailImageJSON_PIR);
+            System.out.println("END");
+
+            System.out.println("peopleImageBytesProtoService, PIR (Full protobuf)");
+            concurrencyCalls.GeneralTesting(peopleImageBytesProtoService::protoNationalityImage_PIR, peopleImageBytesProtoService::protoPersonImage_PIR, peopleImageBytesProtoService::protoPeopleEmailImage_PIR);
+            System.out.println("END");
+            
+            //
+
+            System.out.println("peopleService (not all json)");
+            concurrencyCalls.GeneralTesting(peopleService::nationality, peopleService::person, peopleService::peopleEmail);
+            System.out.println("END");
+
+            System.out.println("peopleService (JSON)");
+            concurrencyCalls.GeneralTesting(peopleService::nationalityJSON, peopleService::person, peopleService::peopleEmailJSON);
+            System.out.println("END");
+
+            System.out.println("peopleImageService (JSON)");
+            concurrencyCalls.GeneralTesting(peopleImageService::nationalityImageJSON, peopleImageService::personImage, peopleImageService::peopleEmailImageJSON);
+            System.out.println("END");
         }
-        for (Nations nat : Nations.values()) {
-            peopleemailResponses.add(CompletableFuture.supplyAsync(() -> peopleemail.apply(nat), this.executorService));
-        }
-        personResponses.add(CompletableFuture.supplyAsync(() -> person.apply(new PersonRequestDTO("987-56-3201", "509823641", Nations.US)), this.executorService));
 
-        // type inference using var, still statically typed but inferred during compile time here using "var"
-        var nationalityDone = CompletableFuture.allOf(nationalityResponses.toArray(new CompletableFuture[0]));
-        var personDone = CompletableFuture.allOf(personResponses.toArray(new CompletableFuture[0]));
-        var peopleemailDone = CompletableFuture.allOf(peopleemailResponses.toArray(new CompletableFuture[0]));
 
-        try {
-            CompletableFuture.allOf(nationalityDone, personDone, peopleemailDone).join();
-        } catch (CompletionException e) {
-            Throwable cause = e.getCause();
-            System.out.println(cause.getMessage());
-            System.exit(1);
-        }
-
-        long time1 = System.currentTimeMillis();
-
-        long time_taken = time1-time0;
-        System.out.println("GeneralTesting Concurrently: " + time_taken);
     }
-
-    public <T> T Println_SideEffect(T result) {
-        System.out.println(result);
-        return result;
-    }
-
-    public <NationalityResponse,PersonResponse,PeopleemailResponse> void Println_GeneralTesting(Function<Nations,NationalityResponse> nationality, Function<PersonRequestDTO,PersonResponse> person, Function<Nations,PeopleemailResponse> peopleemail) {
-        System.out.println("Running Println_GeneralTesting Concurrently");
-
-        List<CompletableFuture<NationalityResponse>> nationalityResponses = new ArrayList<>();
-        List<CompletableFuture<PersonResponse>> personResponses = new ArrayList<>();
-        List<CompletableFuture<PeopleemailResponse>> peopleemailResponses = new ArrayList<>();
-
-        long time0 = System.currentTimeMillis();
-
-        nationalityResponses.add(CompletableFuture.supplyAsync(() -> nationality.apply(Nations.SG), this.executorService)
-                .thenApply(this::Println_SideEffect));
-        personResponses.add(CompletableFuture.supplyAsync(() -> person.apply(new PersonRequestDTO("987-56-3201", "509823641", Nations.US)), this.executorService)
-                .thenApply(this::Println_SideEffect));
-        peopleemailResponses.add(CompletableFuture.supplyAsync(() -> peopleemail.apply(Nations.SG), this.executorService)
-                .thenApply(this::Println_SideEffect));
-        for (Nations nat : Nations.values()) {
-            nationalityResponses.add(CompletableFuture.supplyAsync(() -> nationality.apply(nat), this.executorService)
-                    .thenApply(this::Println_SideEffect));
-        }
-        for (Nations nat : Nations.values()) {
-            peopleemailResponses.add(CompletableFuture.supplyAsync(() -> peopleemail.apply(nat), this.executorService)
-                    .thenApply(this::Println_SideEffect));
-        }
-        personResponses.add(CompletableFuture.supplyAsync(() -> person.apply(new PersonRequestDTO("987-56-3201", "509823641", Nations.US)), this.executorService)
-                .thenApply(this::Println_SideEffect));
-
-        CompletableFuture<Void> nationalityDone = CompletableFuture.allOf(nationalityResponses.toArray(new CompletableFuture[0]));
-        CompletableFuture<Void> personDone = CompletableFuture.allOf(personResponses.toArray(new CompletableFuture[0]));
-        CompletableFuture<Void> peopleemailDone = CompletableFuture.allOf(peopleemailResponses.toArray(new CompletableFuture[0]));
-
-        try {
-            CompletableFuture.allOf(nationalityDone, personDone, peopleemailDone).join();
-        } catch (CompletionException e) {
-            Throwable cause = e.getCause();
-            System.out.println(cause.getMessage());
-            System.exit(1);
-        }
-
-        long time1 = System.currentTimeMillis();
-
-        long time_taken = time1-time0;
-        System.out.println("Println_GeneralTesting Concurrently: " + time_taken);
-    }
-
 
 }
